a53d888d972549aba0a5d8efdb673946bde3a0e6
Arghya Patra <arghyapatra33@gmail.com>
2025-07-14 19:19:34 +0530

    60 is complete

---
+++ vedio_60_Strings/index.js
@@ -0,0 +1,25 @@
+console.log ("This is string toutorial!")
+let a = "Arghya";
+
+console.log(a[0])
+console.log(a[1])
+console.log(a[2])
+console.log(a[3])
+console.log(a[4])
+
+console.log (a.length)
+
+let real_name = "Arghya"
+let friend = "rohan"
+console.log("His name is "+ real_name +" and is friend name is" + friend)
+console.log(`His name is ${real_name} and his friend name is ${friend}`)
+  
+
+let b = "patra"
+console.log(b.toUpperCase())
+console.log(b.toLowerCase())
+console.log(b.slice(1,4))
+console.log(b.slice(1))
+console.log(b.replace("ra", "33"))
+console.log(b.concat(a, "sneha", "kholi", "dhoni"))
+console.log(b) 

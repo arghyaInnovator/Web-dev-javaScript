92c84da95f676f46ea8518daca3c5a04cac224d3
Arghya Patra <arghyapatra33@gmail.com>
2025-07-21 00:11:47 +0530

    68 complete

---
+++ 68_JS_Events/script.js
@@ -0,0 +1,14 @@
+let button = document.getElementById("btn")
+
+
+button.addEventListener("dblclick", ()=>{
+    document.querySelector(".box").innerHTML = "<b>Yayy you were clicked</b> Enjoy your click!"
+})
+
+button.addEventListener("contextmenu", ()=>{
+    alert("Dont hack us by Right click Please")
+})
+
+document.addEventListener("keydown", (e)=>{
+    console.log(e, e.key, e.keyCode)
+})
